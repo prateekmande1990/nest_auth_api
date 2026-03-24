00b1640a1c974c3b7f9a676de4518954ac8e1ec3
Pratik Mande <prateekmande@gmail.com>
2026-03-24 14:21:14 +0530

    add user module and service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { RegisterDto } from './dto/register.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
 export class AuthService {
 
-  constructor() {}
+  constructor(private readonly userService: UserService) { }
 
   register(registerDto: RegisterDto) {
 
-    return registerDto
+    // 1 check email already exists
+    // 2 hash password
+    // 3 create user
+    // 4 generate jwt token
+    // 5 return jwt token
+
+    const user = this.userService.getUserByEmail(registerDto.email)
+
+    return user
 
   }
 }
---
+++ src/user/user.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class UserService {
+
+
+  getUserByEmail(email: string) {
+    return {email}
+  }
+}
